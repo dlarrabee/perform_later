dadfcdf360be45d2e4cda2a021212c7556ec7793
David Larrabee <david.larrabee@meyouhealth.com>
2014-02-05 15:14:51 -0500

    Do not send empty array , assume no parameters

--- lib/perform_later/workers/base.rb
+++ lib/perform_later/workers/base.rb
@@ -1,19 +1,23 @@
 module PerformLater
   module Workers
-		class Base
-      
-  		protected
-  			def self.perform_job(object, method, arguments)
-  				unless arguments.empty?
-            if arguments.size == 1
-              object.send(method, arguments.first)
-            else
-              object.send(method, *arguments)
-            end
+    class Base
+
+      protected
+      def self.perform_job(object, method, arguments)
+        unless arguments_empty?(arguments)
+          if arguments.size == 1
+            object.send(method, arguments.first)
           else
-            object.send(method)
+            object.send(method, *arguments)
           end
-  			end
-  		end
-	end
+        else
+          object.send(method)
+        end
+      end
+
+      def self.arguments_empty?(arguments)
+        arguments.empty? || (arguments.size == 1 && arguments.first.is_a?(Array) && arguments.first.empty?)
+      end
+    end
+  end
 end
